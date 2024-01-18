db68885d2220c1999eded4b4f932b837d1557026
Danesh Naik <naikdanesh2@gmail.com>
2024-01-18 13:46:27 +0530

    updated now

--- app/src/main/java/com/freelancing/zhenisapp/MainActivity.java
+++ app/src/main/java/com/freelancing/zhenisapp/MainActivity.java
@@ -1,19 +1,78 @@
 package com.freelancing.zhenisapp;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.os.Build;
 import android.os.Bundle;
-import android.view.TextureView;
+import android.view.Surface;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.camera.core.CameraSelector;
+import androidx.camera.core.Preview;
+import androidx.camera.lifecycle.ProcessCameraProvider;
+
+import com.freelancing.zhenisapp.R;
+import com.google.common.util.concurrent.ListenableFuture;
 
 public class MainActivity extends AppCompatActivity {
-    Pre
+
+    private ListenableFuture<ProcessCameraProvider> cameraProviderFuture;
+    private SurfaceView surfaceView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        surfaceView = findViewById(R.id.cameraSurfaceView);
+        cameraProviderFuture = ProcessCameraProvider.getInstance(this);
 
+        surfaceView.getHolder().addCallback(new SurfaceHolder.Callback() {
+            @Override
+            public void surfaceCreated(@NonNull SurfaceHolder holder) {
+                startCamera();
+            }
 
+            @Override
+            public void surfaceChanged(@NonNull SurfaceHolder holder, int format, int width, int height) {
+                // Handle surface changes if needed
+            }
+
+            @Override
+            public void surfaceDestroyed(@NonNull SurfaceHolder holder) {
+                // Release camera resources if needed
+            }
+        });
     }
+
+    private void startCamera() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.P) {
+            cameraProviderFuture.addListener(() -> {
+                try {
+                    ProcessCameraProvider cameraProvider = cameraProviderFuture.get();
+
+                    // Set up the Preview use case
+                    Preview preview = new Preview.Builder().build();
+                    preview.setSurfaceProvider(holder -> {
+                        Surface surface = holder.getSurface();
+                        // Use the surface as needed
+                    });
+
+                    // Select the back camera as a default
+                    CameraSelector cameraSelector = CameraSelector.DEFAULT_BACK_CAMERA;
+
+                    // Unbind any existing use cases before rebinding
+                    cameraProvider.unbindAll();
+
+                    // Bind the camera use cases to the preview
+                    cameraProvider.bindToLifecycle(this, cameraSelector, preview);
+
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }, getMainExecutor());
+        }
+    }
+
+
 }
